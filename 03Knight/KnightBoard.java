a31b63d5bd2fd55db7a566f962efc5401850afd2
djiang2 <djiang2>
2018-02-14 15:28:09 -0500

    Added moveset

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,5 +1,23 @@
 public class KnightBoard{
     private int[][] board;
+    private final int[] moveX = new int[4];
+    moveX[0] = -1;
+    moveX[1] = 1;
+    moveX[2] = -2;
+    moveX[3] = 2;
+    moveX[4] = -1;
+    moveX[5] = 1;
+    moveX[6] = -2;
+    moveX[7] = 2;
+    private final int[] moveY = new int[4];
+    moveX[0] = -1;
+    moveX[1] = 1;
+    moveX[2] = -2;
+    moveX[3] = 2;
+    moveX[4] = -1;
+    moveX[5] = 1;
+    moveX[6] = -2;
+    moveX[7] = 2;
     
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
@@ -16,6 +34,13 @@ public class KnightBoard{
 		}
 	    }
 	}
+	return true; //solveH(int row, int col, int level)
+    }
+
+    private boolean solveH(int row, int col, int level){
+	if(!solveH(row,col,level)){
+
+	}
     }
     
     public String toString(){
@@ -24,14 +49,17 @@ public class KnightBoard{
 	    for(int j = 0; j < board[i].length; j++){
 		if(board[i][j] == 0){
 		    result += "  -";
-			} else{
+			} else
+		    if(board[i][j] < 10){
 	result += "  " + board[i][j];
+		    }else{
+			result += " " + board[i][j];
 	    }
 	    }
 	    result += "\n";
 	}
 	return result;
-    }
+	}
     
     public static void main(String[]args){
 	KnightBoard test = new KnightBoard(5,5);
