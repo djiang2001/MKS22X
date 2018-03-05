ce4d2b3aa63d883582279e9d69d19eff5dd8d0a0
djiang2 <djiang2>
2018-03-05 15:27:12 -0500

    USACO

---
+++ 05USACO/USACO.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class USACO{
+
+    public static int bronze(String filename){
+	int[][] lake = [][]
+	    File f = new File(filename);
+	Scanner inf = new Scanner(f);
+	
+	
+    }
+
+    public static int silver(String filename){
+
+    }
+    
+}
