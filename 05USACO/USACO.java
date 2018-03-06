8e383e78753d9ef5cf736ff09426f7d97518e74d
djiang2 <djiang2>
2018-03-06 15:27:40 -0500

    more USACo

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -3,10 +3,20 @@ import java.io.*;
 public class USACO{
 
     public static int bronze(String filename){
-	int[][] lake = [][]
+	try{
+	    int r = 0;
+	    int c = 0;
 	    File f = new File(filename);
-	Scanner inf = new Scanner(f);
-	
+	    Scanner inf = new Scanner(f);
+	    while(inf.hasNextLine()){
+		String s = inf.nextLine();
+		for(int i = 0; i < s.length(); i++){
+
+		}
+	    }
+	}
+	catch(){
+	}
 	
     }
 
