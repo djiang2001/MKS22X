6f6032d4eb3e7d88756d224b551197d87a4c37ab
djiang2 <djiang2>
2018-03-20 15:28:51 -0400

    Merge

---
+++ 07Merge/Merge.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Merge{
+    
+    public static void mergesort(int[] data){
+	int[] temp = new int[data.length];
+	msort(data,temp,data[0],data[data.length-1]);
+    }
+    
+    private static void msort(int[] data, int[] temp,int lo, int hi){
+	for(int i = lo; i <= hi; i++){
+	    temp[i] = data[i];
+	}
+	if(lo >= hi){
+	    return;
+	}
+	int mid = (lo + hi)/ 2;
+	msort(temp, data, lo, mid);
+	msort(temp, data, mid + 1, hi);
+	merge(data, temp, lo, mid, hi);
+    }
+
+    public static void merge(int[] data, int[] temp, int lo, int mid, int hi){
+	for(int i = 0; i <= hi; i++){
+	    if(temp[lo] < temp[mid]){
+		data[i] = temp[lo];
+		lo++;
+	    }else if(temp[mid] < temp[lo]){
+		data[i] = temp[mid];
+		mid++;
+	    }
+	}
+    }
+    
+    public static void main(String[] args){
+	int[] test ={1,6,3,8,5,6,7,5,7,8,5,4,2};
+        mergesort(test);
+	System.out.println(Arrays.toString(test));
+    }
+}
