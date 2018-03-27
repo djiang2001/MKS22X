ffbdb663957eb8fe09d2d8da5d17c20074ca7b06
djiang2001 <djiang2@stuy.edu>
2018-03-26 22:11:43 -0400

    Edited mergesort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -4,14 +4,14 @@ public class Merge{
     
     public static void mergesort(int[] data){
 	int[] temp = new int[data.length];
+	for(int i = 0; i < data.length; i++){
+	    temp[i] = data[i];
+	}
 	msort(data,temp,0,data.length-1);
     }
     
     private static void msort(int[] data, int[] temp,int lo, int hi){
 	if(lo < hi){
-	    for(int i = lo; i <= hi; i++){
-		temp[i] = data[i];
-	    }
 	   int mid = (lo + hi)/ 2;
 	    msort(temp, data, lo, mid);
 	    msort(temp, data, mid + 1, hi);
