e107bd54f7013aa35447dab1089393c333251c6f
djiang2 <djiang2>
2018-04-18 15:32:17 -0400

    Working on calculator

--- 09StackCalc/Calculator.java
+++ 09StackCalc/Calculator.java
@@ -2,10 +2,25 @@ import java.util.*;
 
 public class Eval{
     public static double eval(String s){
-
-
+	Stack stack = new Stack();
+	String operator = "+-*/%";
+	String[] holder = s.split(" ");
+	for(int i = 0; i < holder.length; i++){
+	    if(holder[i].equals("+")){
+		stack.push(stack.pop() + stack.pop());
+	    } 
+	    if(holder[i].equals("-")){
+		stack.push((stack.pop() * -1) + stack.pop());
+	    }
+	    if(holder[i].equals("*")){
+		stack.push(stack.pop() * stack.pop());
+	    }
+	    if(holder[i].equals("/")){
+		stack.push((1/stack.pop()) * stack.pop());
+	    }
+	    if(holder[i].equals("%")){
+		stack.push()
+	    }
+	}
     }
-
-
-
 }
---
+++ 09StackCalc/Calculator.java~
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Eval{
+    public static double eval(String s){
+
+
+    }
+
+
+
+}
