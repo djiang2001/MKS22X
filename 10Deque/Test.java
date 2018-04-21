ad2eff02d9e4cd2c2e6fd6bc53756cc266ba711c
djiang2001 <djiang2@stuy.edu>
2018-04-21 15:14:26 -0400

    tested with crystal's driver

---
+++ 10Deque/Test.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+public class <E>{
+  public E[] data;
+  public int size,front,back;
+
+  public MyDeque() {
+    this(10);
+  }
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity) {
+    if (initialCapacity < 0){
+      throw new IllegalArgumentException();
+    }
+    data = (E[]) new Object[initialCapacity];
+    size = 0;
+    front = 0;
+    back = 0;
+  }
+
+  public String toString() {
+    return Arrays.toString(data);
+  }
+
+  public int size() {
+    return size;
+  }
+
+  @SuppressWarnings("unchecked")
+  public void resize() {
+    E[] newArr = (E[])new Object[size*2];
+    for (int i=0; i<data.length; i++){
+      newArr[i] = data[(i+front)%data.length];
+    }
+    data = newArr;
+    front = 0;
+    back = size-1;
+  }
+
+  public void addLast(E value) {
+    if (value == null){
+      throw new NullPointerException();
+    }
+    if (size == data.length){
+      resize();
+    }
+    if (size != 0){
+      back = (back+1) % data.length;
+    }
+    data[back] = value;
+    size++;
+  }
+
+  public E removeLast() {
+    if (size < 1) {
+      throw new NoSuchElementException();
+    }
+    E cur = data[back];
+    data[back] = null;
+    if (size > 1){
+      back = (back-1+data.length) % data.length;
+    }
+    size--;
+    return cur;
+  }
+
+  public void addFirst(E value){
+    if (value == null){
+      throw new NullPointerException();
+    }
+    if (size == data.length){
+      resize();
+    }
+    if (size != 0){
+        front = (front-1+data.length) % data.length;
+    }
+    data[front] = value;
+    size++;
+  }
+
+  public E removeFirst(){
+    if (size < 1) {
+      throw new NoSuchElementException();
+    }
+    E cur = data[front];
+    data[front] = null;
+    if (size > 1){
+      front = (front+1) % data.length;
+    }
+    size--;
+    return cur;
+  }
+
+  public E getFirst() {
+    if (size < 1) {
+      throw new NoSuchElementException();
+    }
+    return data[front];
+  }
+
+  public E getLast() {
+    if (size < 1) {
+      throw new NoSuchElementException();
+    }
+    return data[back];
+  }
+}
