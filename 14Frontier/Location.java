8f1b1106d01116e2bc38d52e11eb3a5a9f26b286
djiang2 <djiang2>
2018-05-07 14:46:27 -0400

    New methods in maze

--- 14Frontier/Location.java
+++ 14Frontier/Location.java
@@ -3,5 +3,16 @@ public class Location{
     private Location previous;
 
     public Location(int _x, int _y, Location prev){
+	x = _x;
+	y = _y;
+	previous = prev;
+    }
+
+    public static int getX(){
+	return x;
+    }
+
+    public static int getY(){
+	return y;
     }
 }
