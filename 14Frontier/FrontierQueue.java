4dc10d8b87f01f301fa38d056e54b780a0848439
djiang2001 <djiang2@stuy.edu>
2018-05-08 20:37:55 -0400

    Worked on frontier, added distance in location

--- 14Frontier/FrontierQueue.java
+++ 14Frontier/FrontierQueue.java
@@ -3,13 +3,19 @@ import java.util.*;
 public class FrontierQueue implements Frontier{
     public Queue<Location> locations;    
 
-    public FrontierQueue(int ex,int why,int pleb){
-	super(ex,why,pleb);
+    public FrontierQueue(){
+	locations = new LinkedList<Location>();
+    }
+    
+    public Location next(){
+	return locations.remove();
+    }
+    
+    public void add(Location l){
+	locations.add(l);
     }
 
-    public void add(Location){
-	locations.add(Location);
-    }
-
+    public boolean hasNext(){
+	return 
     
 }
