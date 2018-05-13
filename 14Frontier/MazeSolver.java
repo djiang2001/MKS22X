ab9b805aa5794b1a1aa26481682c2ea8f5dfd279
djiang2001 <djiang2@stuy.edu>
2018-05-13 17:25:46 -0400

    Animation isnt working

--- 14Frontier/MazeSolver.java
+++ 14Frontier/MazeSolver.java
@@ -1,9 +1,13 @@
+import java.util.*;
+
 public class MazeSolver{
     private Maze maze;
     private Frontier frontier;
+    private boolean animate;
 
     public MazeSolver(String mazeText){
-    
+	maze = new Maze(mazeText);
+	animate = false;
     }
 
     //Default to BFS
@@ -15,29 +19,69 @@ public class MazeSolver{
     //2: Priority
     public boolean solve(int mode){
 	if(mode == 0){
-	frontier = new FrontierQueue;
-	while(frontier.hasNext()){
-
+	    frontier = new FrontierQueue();
 	}
-	}
-	}
-
-	if(mode == 1){
+	else if(mode == 1){
 	    frontier = new FrontierStack();
-	    while(frontier.hasNext()){
+	}
+	/*	else if(mode == 2){
+	    frontier = new FrontierPriorityQueue();
+	}
+	*/
+
+	Location next = maze.getStart();
+	Location end = maze.getEnd();
+
+	frontier.add(next);
+	while(frontier.hasNext()){
+	    next = frontier.next();
+	    maze.set(next.getX(),next.getY(),'@');
+
+	    Location[] neighbors = maze.getNeighbors(next);
+
+	    for(int i = 0; i < neighbors.length; i++){
+		if(neighbors[i] != null){
+		    if(neighbors[i].equals(end)){
+			maze.set(maze.getStart().getX(),maze.getStart().getY(),'S');
+			maze.set(maze.getEnd().getX(),maze.getEnd().getY(),'E');
+			return true;
+		    } else {
+		    frontier.add(neighbors[i]);
+		    maze.set(neighbors[i].getX(),neighbors[i].getY(),'.');
+		    }
+		}
 	    }
 	}
-	//while there is stuff in the frontier:
-	//  get the next location
-	//  process the location to find the locations (use the maze to do this)
-	//  check if any locations are the end, if you found the end just return true!
-	//  add all the locations to the frontier
-	//when there are no more values in the frontier return false
 	return false;
     }
 
     public String toString(){
 	return maze.toString();
+    }
+
+    private void wait(int millis){
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
+    }
+    
+    public void setAnimate(boolean b){
+	animate = b;
+    }
+    
+    public void clearTerminal(){
+	//erase terminal, go to top left of screen.
+	System.out.println("\033[2J\033[1;1H");
+    }
+
+    public static void main(String [] args){
+	MazeSolver test = new MazeSolver("test.txt");
+	System.out.println(test);
+	test.setAnimate(true);
+	test.solve();
+	System.out.println(test);
     }
 }
 
---
+++ 14Frontier/MyHeap.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+public class MyHeap<T extends Comparable<T>>{
+    private int length;
+    private T[] data;
+    private boolean isMax;
+
+    
+    MyHeap(){
+	this(true);
+    }
+
+    @SuppressWarnings("unchecked")
+    MyHeap(boolean other){
+	if(other){
+	    isMax = true;
+	}else {
+	    isMax = false;
+	}
+	length = 0;
+	data = (T[]) new Comparable[10];
+
+    }
+
+    public void swap(int index1, int index2){
+	T temp = data[index1];
+	data[index1] = data[index2];
+	data[index2] = temp;
+    }
+
+    public void pushup(int index){
+	if((isMax && data[index].compareTo(data[(index-1)/2]) > 0) ||
+	   (!isMax && data[index].compareTo(data[(index-1)/2]) < 0)){
+	    swap(index, (index -1)/2);
+	    pushup((index-1)/2);
+	}
+    }
+
+    public void pushdown(int index){
+	int first = 2*index + 1;
+	int sec = 2*index + 2;
+	if(first < size() && sec < size()){
+	    if(isMax && data[index].compareTo(data[first]) < 0 ||
+	       !isMax && data[index].compareTo(data[first]) > 0){
+		swap(index,first);
+		pushdown(first);
+	    }else
+		if(isMax && data[index].compareTo(data[sec]) < 0 ||
+		   !isMax && data[index].compareTo(data[sec]) > 0){
+		    swap(index,sec);
+		    pushdown(sec);
+		}
+	}
+    }
+    
+    public void add(T str){
+	if (size() == data.length){
+	    resize();
+	}
+	data[size()] = str;
+	pushup(size());
+	length++;
+    }
+
+    public T remove(){
+	T str = peek();
+	swap(0, size()-1);
+	length--;
+	pushdown(0);
+	return str;
+    }
+
+    public T peek(){
+	return data[0];
+    }
+
+    public int size(){
+	return length;
+    }
+
+      @SuppressWarnings("unchecked")
+    public void resize(){
+	T[] temp = (T[]) new Comparable[2*size()];
+	for(int i = 0; i < size(); i++){
+	    temp[i] = data[i];
+	}
+	data = temp;
+    }
+
+    public String toString(){
+	String result = "";
+	for(int i = 0; i < size(); i++){
+	    result += "[" + data[i] + "]";
+	}
+	return result;
+    }
+
+       public static void main(String[]args){
+    }
+}
+
